d9e9cdbf18d572eb9afb9bc5fc5e81843cae412d
CharlesVigo <charlesvigo8@gmail.com>
2024-06-24 16:30:26 +0800

    database connection added

---
+++ backend/router/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+//sign up
+//@route /api/user/signup
+//login
+//@route /api/user/login
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+//database connection
+const dbConnection = process.env.DBCONNECTION;
+mongoose
+  .connect(dbConnection)
+  .then(()=>{
+    console.log("Connected to database")
 
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT,()=>{
+      console.log(`Server is running on ${PORT}`)
+    })
 
+  })
+  .catch((err)=>{
+    console.log('Error connection to the database',err)
+  })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-  console.log(`Server is running on ${PORT}`)
-})
