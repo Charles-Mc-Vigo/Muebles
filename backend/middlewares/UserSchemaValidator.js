a6d15d4b19971099e248124005b4db8d7c74b1b2
CharlesVigo <charlesvigo8@gmail.com>
2024-07-14 23:39:02 +0800

    gender added to user model

--- backend/middlewares/UserSchemaValidator.js
+++ backend/middlewares/UserSchemaValidator.js
@@ -3,6 +3,7 @@ const joi = require("joi");
 const UserSchemaValidator = joi.object({
   firstname: joi.string().required().min(3).max(30),
   lastname: joi.string().required().min(3).max(30),
+  gender:joi.string().required().valid("Male","Female"),
   streetAddress: joi.string().required(),
   municipality: joi.string().required().valid("Boac", "Gasan", "Torrijos", "Buenavista", "Mogpog", "Santa Cruz"),
   password: joi.string().required(),
