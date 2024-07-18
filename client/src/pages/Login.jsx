0130f1b4a58544d214183b202b44e72e73406580
CharlesVigo <charlesvigo8@gmail.com>
2024-07-18 13:05:19 +0800

    Login form done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link, useNavigate} from 'react-router-dom';
+import axios from 'axios';
+
 
 export default function Login() {
+	const [formData,setFormData] = useState({});
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		const {id,value}= e.target;
+		setFormData({...formData, [id]: value});
+		console.log(formData);
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			const response = await axios.post("http://localhost:3000/api/users/login",formData);
+			console.log(response.data);
+			alert("Login successfully!");
+			navigate("/home");
+		} 
+			catch (error) {
+				console.error("Login error", error.response?.data || error.message);
+				alert(error.response?.data?.message || error.message || "Login failed");
+		}
+	}
 	return (
 		<div className="p-3 max-w-lg mx-auto">
-			<h1 className="text-center font-semibold my-7 text-3xl">Welcome to Muebles!</h1>
-			<form className="flex flex-col gap-4">
+			<h1 className="text-center font-semibold my-7 text-3xl">Welcome to JCKAME Furniture Shop!</h1>
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
 				<input
 					type="email"
 					placeholder="Email"
 					id="email"
 					required
 					className="bg-slate-100 p-3 rounded-lg"
+					onChange={handleChange}
 				/>
 				<input
 					type="password"
@@ -19,6 +45,8 @@ export default function Login() {
 					id="password"
 					required
 					className="bg-slate-100 p-3 rounded-lg"
+					onChange={handleChange}
+
 				/>
 
 				<button
