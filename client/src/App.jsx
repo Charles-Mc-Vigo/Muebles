a47de026266a5632d6ca25628a1d13bab3d46f90
CharlesVigo <charlesvigo8@gmail.com>
2024-07-22 15:52:42 +0800

    frontend update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import AdminLogin from "./pages/AdminLogin";
-import ProtectedRoute from "./components/protectedRoutes";
 
 export default function App() {
   return (
@@ -16,22 +15,10 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminLogin />} />
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute admin={true}>
-            <Dashboard/>
-          </ProtectedRoute>
-        }>
-        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
+
+
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+	return (
+		<div className="lg:max-w-7xl bg-slate-400 mx-auto h-screen flex justify-center items-center">
+			<h1 className="text-center text-5xl">Admin Dashboard</h1>
+		</div>
+	);
 }
