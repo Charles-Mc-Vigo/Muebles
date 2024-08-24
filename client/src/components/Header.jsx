0b6efc1d3ba4e6c9ea1fa223b96ad42a20964912
CharlesVigo <charlesvigo8@gmail.com>
2024-08-24 13:57:08 +0800

    furniture list page added to app.jsx file

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -53,7 +53,7 @@ export default function Header({ showLogin = false }) {
               </button>
               {isHomeFurnitureDropdownOpen && (
                 <ul className="absolute left-0 mt-2 w-40 bg-white border border-gray-200 rounded-md shadow-lg z-50 font-sans">
-                  <Link to="/Furniture-list">
+                  <Link to="/furniture-list">
                     <li className="px-4 py-2 hover:bg-gray-100">All Furnitures </li>
                   </Link>
                   <Link to="/livingroom">
