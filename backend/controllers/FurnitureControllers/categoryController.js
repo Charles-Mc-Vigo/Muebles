c946b4a402fa310b04a5fccfd0d467e09949a59a
CharlesVigo <charlesvigo8@gmail.com>
2024-09-30 10:33:30 +0800

    category controller modified

--- backend/controllers/FurnitureControllers/categoryController.js
+++ backend/controllers/FurnitureControllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../../models/Furniture/categoryModel");
+const FurnitureType = require("../../models/Furniture/furnitureTypeModel");
 
 exports.AddCategory = async(req,res)=>{
   try {
@@ -44,4 +45,26 @@ exports.getCategoryById = async(req,res)=>{
   }
 }
 
+
+
+exports.getAllTypesInCategory = async(req,res)=>{
+  try {
+    const {categoryId} = req.params;
+    const category = await Category.findById(categoryId);
+
+    if(!category){
+      return res.status(404).json({message:"Category not found!"})
+    }
+
+    const furnitureType = await FurnitureType.find({categoryId:category._id});
+    if(furnitureType.length===0){
+      return res.status(404).json({message:`No furniture types found in ${category.name}`})
+    }
+    res.status(200).json(furnitureType)
+
+  } catch (error) {
+    console.log("Error fetching the categories: ",error);
+    res.status(500).json({message:"Server error!"})
+  }
+}
 
