48e410c3554fd4615ae430f00d0539332c00f29f
CharlesVigo <charlesvigo8@gmail.com>
2024-10-03 07:40:52 +0800

    rgb and hex color added to color model

--- backend/controllers/FurnitureControllers/colorController.js
+++ backend/controllers/FurnitureControllers/colorController.js
@@ -30,11 +30,27 @@ exports.getColors = async (req,res) => {
   }
 }
 
-exports.editColor = async (req,res) => {
+exports.editColor = async (req, res) => {
   try {
-    co
+    const { colorId } = req.params;
+    const color = await Colors.findById(colorId);
+    if (!color) return res.status(404).json({ message: "Color not found!" });
+
+    const { name, rgb, hex } = req.body;
+    if (!name || !rgb || !hex) return res.status(400).json({ message: "All fields are required!: name, rgb, hex" });
+
+    // Update the color fields
+    color.name = name;
+    color.rgb = rgb;
+    color.hex = hex;
+
+    // Save the updated color
+    await color.save();
+
+    console.log(color);
+    res.status(200).json({ message: `${color.name} updated successfully!` });
   } catch (error) {
-    console.log("Error editing color: ",error);
-    res.status(500).json({message:"Server error!"})
+    console.log("Error editing color: ", error);
+    res.status(500).json({ message: "Server error!" });
   }
 }
